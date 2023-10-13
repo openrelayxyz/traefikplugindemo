e9af5f731235fa9185cafa7ebc4f99dcc965701e
Austin Roberts <austin.roberts@rivet.cloud>
2023-10-13 12:04:38 -0500

    Initial customization commit
    This is a proof-of-concept. For anything intended for production, I'll
    clean things up considerably.
    
    The basic idea behind this plugin is to read RPC calls, inspect the method,
    rewrite the path based on the method, and send the call back to traefik to
    be routed to a service.
    
    I'm working on a provider plugin to generate the config, but for now there's
    a sample in the cfg/ directory.

--- demo.go
+++ demo.go
@@ -4,63 +4,94 @@ package plugindemo
 import (
 	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"net/url"
 	"text/template"
+	"io/ioutil"
+	"log"
+	"strings"
 )
 
 // Config the plugin configuration.
 type Config struct {
-	Headers map[string]string `json:"headers,omitempty"`
+	// Providers map[string]string `json:"providers,omitempty"`
+	// Methods map[string][]string `json:"methods,omitempty"`
 }
 
 // CreateConfig creates the default plugin configuration.
 func CreateConfig() *Config {
 	return &Config{
-		Headers: make(map[string]string),
+		// Providers: make(map[string]string),
+		// Methods: make(map[string][]string),
 	}
 }
 
+// type methodChoice struct {
+// 	counter int
+// 	methods []*url.URL
+// }
+
+// func (mc *methodChoice) next() *url.URL {
+// 	mc.counter++
+// 	return mc.methods[mc.counter % len(mc.methods)]
+// }
+
 // Demo a Demo plugin.
 type Demo struct {
-	next     http.Handler
-	headers  map[string]string
-	name     string
-	template *template.Template
+	client    *http.Client
+	next      http.Handler
+	// methods   map[string]*methodChoice
+	name      string
+	template  *template.Template
 }
 
 // New created a new Demo plugin.
 func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
-	if len(config.Headers) == 0 {
-		return nil, fmt.Errorf("headers cannot be empty")
-	}
 
 	return &Demo{
-		headers:  config.Headers,
+		client: &http.Client{},
 		next:     next,
 		name:     name,
 		template: template.New("demo").Delims("[[", "]]"),
 	}, nil
 }
 
+type rpcCall struct {
+	Method string `json:"method"`
+}
+
 func (a *Demo) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	for key, value := range a.headers {
-		tmpl, err := a.template.Parse(value)
-		if err != nil {
-			http.Error(rw, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		writer := &bytes.Buffer{}
-
-		err = tmpl.Execute(writer, req)
-		if err != nil {
-			http.Error(rw, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		req.Header.Set(key, writer.String())
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		log.Printf("Error reading body: %v", err)
+		http.Error(rw, "Server Error1", http.StatusInternalServerError)
+		return
 	}
+	var rpc rpcCall
+	if err := json.Unmarshal(body, &rpc); err != nil {
+		log.Printf("Error unmarshalling")
+		a.next.ServeHTTP(rw, req)
+		return
+	}
+
+	path := "/" + strings.Join(strings.Split(rpc.Method, "_"), "/")
+	
+	req.URL.RawPath = path
+	
+	req.URL.Path, err = url.PathUnescape(req.URL.RawPath)
+	if err != nil {
+		// middlewares.GetLogger(context.Background(), r.name, typeName).Error().Err(err).Send()
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	req.Body = ioutil.NopCloser(bytes.NewBuffer(body))
+	
+	req.RequestURI = req.URL.RequestURI()
+	
+	log.Printf("Path %v", req.RequestURI)
 
 	a.next.ServeHTTP(rw, req)
 }
